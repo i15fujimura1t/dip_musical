9a7a66f4f7e783c0cbe0af263e95c1d2ffd36133
i15fujimura1t <49701015+i15fujimura1t@users.noreply.github.com>
2021-10-12 18:29:03 +0900

    Update dip.py

--- dip.py
+++ dip.py
@@ -1,11 +1,6 @@
-import sys
-sys.path.append('/content/drive/My Drive/code2020/mymodules/')
-sys.path.append('/content/drive/My Drive/code2020/miyazakilab/')
 import torch
-from util.features import *
-from util.wav import *
-from util.plot import *
-from network.unet import *
+from util import *
+from unet import *
 
 def pad32(x):
     shape = x.shape
@@ -14,11 +9,11 @@ def pad32(x):
     m = nn.ZeroPad2d((pad2, 0, pad1, 0))
     return m(x), pad1, pad2
 
-def dip_musical(s_hat, num_iter=1000, avg_num=1, isrelu=False, plot_every=200, seed=123, device='cuda:0'):
-    S_hat = stft(s_hat, win=np.hamming, fftl=512, shift=128)
-    As_hat = np.abs(S_hat)
-    As_hat[As_hat==0] = np.spacing(1)
-    target = torch.from_numpy(As_hat).clone()
+def dip_musical(s_musical, num_iter=1000, avg_num=1, plot_every=100, isrelu=False, device='cuda:0', learning_rate=0.01, seed=1234):
+    S_musical = stft(s_musical, win=np.hamming, fftl=512, shift=128)
+    As_musical = np.abs(S_musical)
+    As_musical[As_musical==0] = np.spacing(1)
+    target = torch.from_numpy(As_musical).clone()
     target = torch.unsqueeze(target, 0)
     target = torch.unsqueeze(target, 0)
     target, pad1, pad2 = pad32(target)
@@ -31,26 +26,30 @@ def dip_musical(s_hat, num_iter=1000, avg_num=1, isrelu=False, plot_every=200, s
 
     params = []
     params += [x for x in model.parameters() ]
-    optimizer = torch.optim.Adam(params, lr=0.01)
+    optimizer = torch.optim.Adam(params, lr=learning_rate)
     net_input = torch.rand(size=target.shape)*0.1
     net_input = net_input.to(device)
+
     for i in range(num_iter+1):
         print('\r%d回目' %(i), end='')
         optimizer.zero_grad()
-        out = model(net_input)
+        net_out = model(net_input)
         if plot_every>0 and i%plot_every==0:
-            out_np = out.to('cpu').detach().numpy().copy()
-            out = out.to(device)
+            out_np = net_out.to('cpu').detach().numpy().copy()
+            net_out = net_out.to(device)
             out_np = out_np[:,:,pad1:, pad2:]
             out_mdn = np.median(out_np[:,0], axis=0)
-            proc = istft(out_mdn*np.exp(1j*np.angle(S_hat)), win=np.hamming, fftl=512, shift=128, x_len=len(s_hat))
-            specshow(proc, fig_size=(10, 3), v_min=-100, v_max=40)
-        loss = L1(out, target)
+            proc = istft(out_mdn*np.exp(1j*np.angle(S_musical)), win=np.hamming, fftl=512, shift=128, x_len=len(s_musical))
+            plt.clf()
+            specshow(proc, fig_size=(10, 3), v_min=-100, v_max=40, title='iter. %d' %(i))
+            plt.pause(0.05)
+        loss = L1(net_out,target)
         loss.backward()
         optimizer.step()
-    out = model(net_input)
-    out_np = out.to('cpu').detach().numpy().copy()
+
+    net_out = model(net_input)
+    out_np = net_out.to('cpu').detach().numpy().copy()
     out_np = out_np[:,:,pad1:, pad2:]
     out_mdn = np.median(out_np[:,0], axis=0)
-    proc = istft(out_mdn*np.exp(1j*np.angle(S_hat)), win=np.hamming, fftl=512, shift=128, x_len=len(s_hat))
+    proc = istft(out_mdn*np.exp(1j*np.angle(S_musical)), win=np.hamming, fftl=512, shift=128, x_len=len(s_musical))
     return proc
